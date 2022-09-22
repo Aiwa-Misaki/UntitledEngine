5fd44b4521272b39906f948c4c37b7eb57141106
Rilliane <1723072376@qq.com>
2022-09-22 20:10:23 +0800

    添加了一些shader和material的定义

---
+++ src/Material.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 克莉斯丁 on 2022/9/22.
+//
+
+#include "Material.h"
---
+++ src/Material.h
@@ -0,0 +1,14 @@
+//
+// Created by 克莉斯丁 on 2022/9/22.
+//
+
+#ifndef UNTITLEDENGINE_MATERIAL_H
+#define UNTITLEDENGINE_MATERIAL_H
+
+
+class Material {
+
+};
+
+
+#endif //UNTITLEDENGINE_MATERIAL_H
